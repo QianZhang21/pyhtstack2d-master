0b7785f28bdb27b02886c7d6b09bff8d7307c083
Qian Zhang <118332242+QianZhang21@users.noreply.github.com>
2025-03-24 13:13:03 +0800

    Update vaspPmgWriter.py

--- pyhtstack2d/calcSets/vaspPmgWriter.py
+++ pyhtstack2d/calcSets/vaspPmgWriter.py
@@ -102,7 +102,7 @@ class PmgInputWriter:
 
     def write_single_input(self, stf=None):
         assert os.path.isfile(stf), f"File {stf} does not exist."
-        uid = ''.join(os.path.basename(stf).split('-')[0])
+        uid = ''.join(os.path.basename(stf).split('-')[:-1])
         workdir_ = os.path.join(self.workdir, uid)
         if not self.overwrite:
             while os.path.isdir(workdir_):
